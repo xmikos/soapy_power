f95aa5b36a354fe3cfd94b43d8f0f6346ec400de
Michal Krenek (Mikos) <m.krenek@gmail.com>
2017-03-24 03:07:32 +0100

    Disable setting thread_name_prefix in ThreadPoolExecutor (only supported in Python >= 3.6)

--- soapypower/threadpool.py
+++ soapypower/threadpool.py
@@ -4,7 +4,8 @@ import os, queue, concurrent.futures
 class ThreadPoolExecutor(concurrent.futures.ThreadPoolExecutor):
     """ThreadPoolExecutor which allows setting max. work queue size"""
     def __init__(self, max_workers=0, thread_name_prefix='', max_queue_size=0):
-        super().__init__(max_workers or os.cpu_count() or 1, thread_name_prefix)
+        #super().__init__(max_workers or os.cpu_count() or 1, thread_name_prefix)
+        super().__init__(max_workers or os.cpu_count() or 1)
         self.max_queue_size = max_queue_size or self._max_workers * 10
         if self.max_queue_size > 0:
             self._work_queue = queue.Queue(self.max_queue_size)
